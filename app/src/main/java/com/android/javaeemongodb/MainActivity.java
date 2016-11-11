1d1d7a8234a26577a5325af15759c93329e9bf8d
Roman S <romeo97934@gmail.com>
2016-11-11 22:09:15 +0200

    Initial commit

---
+++ app/src/main/java/com/android/javaeemongodb/MainActivity.java
@@ -0,0 +1,12 @@
+package java_mongo.com.android.javaeemongodb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
